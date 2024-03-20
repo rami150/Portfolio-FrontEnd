6da08ac9f08c37b025700576349cace9e29b7ecf
rami150 <ramimongelos@hotmail.com>
2024-03-20 16:10:12 -0300

    cambios

--- funciones js.js
+++
@@ -1,8 +0,0 @@
-function sumarDosNumeros(numero1, numero2) {
-    return numero1 + numero2;
-  }
-  
-  // Ejemplo de uso:
-  const resultado = sumarDosNumeros(5, 7);
-  console.log(resultado); // Esto imprimirá 12 en la consola
-  
